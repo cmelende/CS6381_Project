9ccfc4abedef8d8655449090d0e13bdd3ad3f02c
Sam Hays <7843584+gsamuelhays@users.noreply.github.com>
2021-05-31 12:13:52 -0600

    Noddling with parameters to support mininet

--- gsh_research/demo_publisher_with_notifier.py
+++ gsh_research/demo_publisher_with_notifier.py
@@ -1,15 +1,19 @@
 import zmqmw as zmw
-import logging
+import argparse
 from random import randint
 from time import sleep
 import time
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("brokeraddress", help="IP Address of broker", default="localhost", nargs='?')
+    parser.add_argument('brokerport', help="port of the broker", default="5263", type=int, nargs='?')
+    args = parser.parse_args()
 
-    broker = zmw.Broker(address="localhost", port=5263, brokertype=zmw.Broker.BrokerType.notifier)
+    broker = zmw.Broker(address=args.brokeraddress, port=args.brokerport, brokertype=zmw.Broker.BrokerType.notifier)
     pub = zmw.Publisher(address=zmw.Address("*", 7777))
 
-    zmw.Publisher.register_publisher(broker, "localhost", 7777, ['test_topic']) 
+    zmw.Publisher.register_publisher(broker, "10.0.0.2", 7777, ['test_topic']) 
     pub.register_topic('test_topic')
 
     while True:
--- gsh_research/demo_subscriber_using_notifier.py
+++ gsh_research/demo_subscriber_using_notifier.py
@@ -1,13 +1,24 @@
 import zmqmw as zmw
 import json
+import argparse
+from time import time
 
 def resp_printer(topic, body):
-    print(f"topic:{topic}, body:{body}")
+    #print(f"topic:{topic}, body:{body}")
+    f = float(body[0])
+    n = float(time())
+    sender_timestamp = print(f"10.0.0.1,10.0.0.3,{f},{n},{n-f}")
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("brokeraddress", help="IP Address of broker", default="localhost", nargs='?')
+    parser.add_argument('brokerport', help="port of the broker", default="5263", type=int, nargs='?')
+    args = parser.parse_args()
 
-    broker = zmw.Broker('localhost', 5263, zmw.Broker.BrokerType.notifier)
+    broker = zmw.Broker(args.brokeraddress, args.brokerport, zmw.Broker.BrokerType.notifier)
     topics: dict = json.loads(zmw.Subscriber.get_publishers(broker=broker))
+
+    print(json.dumps(topics,indent=4))
 
     subscribers = []
 
@@ -17,4 +28,4 @@ if __name__ == "__main__":
             s.register_notify(t, resp_printer)
             s.register_sub(t)
         subscribers.append(s)
-    
+ 
