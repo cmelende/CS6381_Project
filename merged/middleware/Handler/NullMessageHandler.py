c4f3b16b84595ec91f9acb28fd9ec1b0f72c9b07
Cory <cory.melendez@outlook.com>
2021-05-31 20:27:57 -0500

    add diagram and null message handler

---
+++ merged/middleware/Handler/NullMessageHandler.py
@@ -0,0 +1,6 @@
+from cmelende_research.N0mq.Broker.handler.MessageHandlerBase import MessageHandlerBase
+
+
+class NullMessageHandler(MessageHandlerBase):
+    def handle_message(self, value: str) -> None:
+        pass
