5067833163b878290b963cce364224eb014672f8
Sam Hays <7843584+gsamuelhays@users.noreply.github.com>
2021-06-01 21:18:42 -0600

    adjust mininet tests

--- gsh_research/demo_publisher_with_notifier.py
+++ gsh_research/demo_publisher_with_notifier.py
@@ -8,16 +8,19 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("brokeraddress", help="IP Address of broker", default="localhost", nargs='?')
     parser.add_argument('brokerport', help="port of the broker", default="5263", type=int, nargs='?')
+    parser.add_argument('localip', help="the ip of _this_ device.", default='127.0.0.1', nargs='?')
+    parser.add_argument('topics', help="comma-separated list of topics (no spaces)", default='test_topic', nargs='?')
     args = parser.parse_args()
 
     broker = zmw.Broker(address=args.brokeraddress, port=args.brokerport, brokertype=zmw.Broker.BrokerType.notifier)
-    pub = zmw.Publisher(address=zmw.Address("*", 7777))
-
-    zmw.Publisher.register_publisher(broker, "10.0.0.2", 7777, ['test_topic']) 
+    pub = zmw.Publisher(address=zmw.Address(args.localip, 7777))
+    zmw.Publisher.register_publisher(broker, args.localip, 7777, ['test_topic']) 
     pub.register_topic('test_topic')
 
-    while True:
+    count = 100
+    while count > 0:
         pub.publish("test_topic", str(time.time()))
-        time.sleep(0.25)
+        time.sleep(0.10)
+        count -= 1
 
 
--- gsh_research/demo_subscriber_using_notifier.py
+++ gsh_research/demo_subscriber_using_notifier.py
@@ -2,23 +2,35 @@ import zmqmw as zmw
 import json
 import argparse
 from time import time
+from uuid import uuid4
+
+
+hostname = None
+localip = None
+brokerip = None
 
 def resp_printer(topic, body):
-    #print(f"topic:{topic}, body:{body}")
     f = float(body[0])
     n = float(time())
-    sender_timestamp = print(f"10.0.0.1,10.0.0.3,{f},{n},{n-f}")
+    with open(f"{brokerip}-{localip}.log", "a+") as file:
+        msg = f"{brokerip},{localip},{f},{n},{n-f}"
+        print(msg)
+        file.write(msg + "\n")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("brokeraddress", help="IP Address of broker", default="localhost", nargs='?')
     parser.add_argument('brokerport', help="port of the broker", default="5263", type=int, nargs='?')
+    parser.add_argument('name', help="name to record this host as", default=str(uuid4()), nargs='?')
+    parser.add_argument('localip', help="local ip of this host", default="127.0.0.1", nargs='?')
     args = parser.parse_args()
+
+    hostname = args.name
+    localip = args.localip
+    brokerip = args.brokeraddress
 
     broker = zmw.Broker(args.brokeraddress, args.brokerport, zmw.Broker.BrokerType.notifier)
     topics: dict = json.loads(zmw.Subscriber.get_publishers(broker=broker))
-
-    print(json.dumps(topics,indent=4))
 
     subscribers = []
 
